d6b4e90d4a36d61a8641ea27b0cef621cd3b18b6
Qui3t0wL <46346911+Qui3t0wL@users.noreply.github.com>
2019-05-24 16:06:18 +0100

    add connect.php

---
+++ admin/connect.php
@@ -0,0 +1,26 @@
+<?php 
+
+	//Include database connection details
+	require_once('db.php');
+
+	//Connect to mysql server
+	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link) {
+		die('Failed to connect to server: ' . mysqli_connect_error());
+	}
+	
+	//Select database
+	$db = mysqli_select_db($link, DB_DATABASE);
+	if(!$db) {
+		die("Unable to select database");
+	}
+	
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysqli_real_escape_string($link, $str);
+	}
+?>
